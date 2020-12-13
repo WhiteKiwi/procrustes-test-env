405d7ffbc64a040fad390eb79e237819671c17bb
whitekiwi <jh145478@gmail.com>
2020-12-14 01:10:13 +0900

    v1.0.0

--- src/utils/docker.ts
+++ src/utils/docker.ts
@@ -17,36 +17,54 @@ export async function teardownDockerContainer(containerName: string) {
 }
 
 // Check if the docker image exists
-export async function isImageExist(imageName:string, tag: string) {
-	const existsImageName = await executeShellCommand(`docker images ${imageName} --format '{{.Tag}}'`)
+export async function isImageExist(imageName: string, tag: string) {
+	const existsImageName = await executeShellCommand(
+		`docker images ${imageName} --format '{{.Tag}}'`,
+	)
 	return existsImageName === tag
 }
 
 // Check if the docker container is running
 export async function isDockerContainerRunning(imageName: string) {
-	return !isEmpty(await executeShellCommand(`docker ps -f 'ancestor=${imageName}' --format '{{.Image}}'`))
+	return !isEmpty(
+		await executeShellCommand(
+			`docker ps -f 'ancestor=${imageName}' --format '{{.Image}}'`,
+		),
+	)
 }
 
 // Build a new Docker image
-export async function buildDockerImage(imageName: string, version: string, path: string) {
+export async function buildDockerImage(
+	imageName: string,
+	version: string,
+	path: string,
+) {
 	await executeShellCommand(`docker build -t ${imageName} ${path}`)
 
 	let tries = 0
-	while (await isImageExist(imageName, version)) {
+	while (!(await isImageExist(imageName, version))) {
 		// Wait up to 2 minutes
-		if (tries++ === 12) throw new FailToBuildDockerImageError(`${imageName}`)
+		if (tries++ === 12) {
+			throw new FailToBuildDockerImageError(`${imageName}`)
+		}
 		await sleep(10000)
 	}
 }
 
 // Run a Docker Container
-export async function runDockerContainer(containerName: string, imageName: string, options: string) {
-	await executeShellCommand(`docker run ${options} --name ${containerName} ${imageName}`)
+export async function runDockerContainer(
+	containerName: string,
+	imageName: string,
+	options: string,
+) {
+	await executeShellCommand(
+		`docker run ${options} --name ${containerName} ${imageName}`,
+	)
 
 	let tries = 0
-	while (!(await isDockerContainerRunning(containerName))) {
-		// Wait up to 30 seconds
-		if (tries++ === 6) throw new FailToRunDockerContainerError(imageName)
+	while (!(await isDockerContainerRunning(imageName))) {
+		// Wait up to 60 seconds
+		if (tries++ === 12) throw new FailToRunDockerContainerError(imageName)
 		await sleep(5000)
 	}
 }
